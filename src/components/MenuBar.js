cf30e882bfce0f4b963c34d860d8a814a17f89a0
Brian <brianhkang1@gmail.com>
2018-11-02 10:40:50 -0400

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -2,32 +2,40 @@ import React from 'react'
 
 const MenuBar = (props) => {
 
-  /*
-
-  The 'a' tags below are the menu items. Think about the way a menu 
+  /* The 'a' tags below are the menu items. Think about the way a menu
   should work. When you click a menu item, the button typically becomes
   'active' to indicate that it is currently selected. How could we achieve
   this programatically? What other behavior do we expect when we click
   on a menu item? Do we need state in this component, and if not, how can
-  this component be made aware of what is currently the active menu item?
+  this component be made aware of what is currently the active menu item? */
 
-  */
+  const handleClick = (event) => {
+    props.changeSelected(event.target.id)
+  }
 
   return (
     <div className="ui four item menu">
-      <a className="item active" id="profile">
+      <a className={"item " + (props.selected === "profile" ? "active" : null)}
+          id="profile"
+          onClick={handleClick}>
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a className={"item " + (props.selected === "profile" ? "active" : null)}
+        id="photo"
+        onClick={handleClick}>
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
+      <a className={"item " + (props.selected === "profile" ? "active" : null)}
+        id="cocktail"
+        onClick={handleClick}>
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
+      <a className={"item " + (props.selected === "profile" ? "active" : null)}
+        id="pokemon"
+        onClick={handleClick}>
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
